2a10218786e6efdde6bf47cf285e2dc2622b12a3
navies <epoon061@uottawa.ca>
2020-05-21 22:03:53 -0400

    Lint code

--- model.py
+++ model.py
@@ -2,9 +2,10 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 import torchvision.models as models
-import copy
+from tqdm import tqdm
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 
 def gram_matrix(x):
     """Computes the gram matrix of the input
@@ -19,6 +20,7 @@ def gram_matrix(x):
     # For input matrices with large dimensions, the gram matrix
     # typically has large values, thus we normalize
     return (features @ features.T) / (a * b * c * d)
+
 
 class Normalization(torch.nn.Module):
     '''Normalization layer
@@ -45,6 +47,7 @@ class Normalization(torch.nn.Module):
     def forward(self, x):
         return (x - self.mean) / self.std
 
+
 class ContentLoss(torch.nn.Module):
     '''A dummy layer to compute the content loss at a given layer
 
@@ -60,11 +63,13 @@ class ContentLoss(torch.nn.Module):
         super(ContentLoss, self).__init__()
         # self.target = target.detach()
         self.target = None
+        self.loss = None
 
     def forward(self, x):
         if self.target is not None:
             self.loss = F.mse_loss(x, self.target)
         return x
+
 
 class StyleLoss(torch.nn.Module):
     '''A dummy layer to compute the style loss at a given layer
@@ -81,15 +86,21 @@ class StyleLoss(torch.nn.Module):
         super(StyleLoss, self).__init__()
         self.targets = None
         self.weights = None
+        self.loss = None
 
     def forward(self, x):
         if self.targets is not None:
-            self.loss = sum([w * F.mse_loss(gram_matrix(x), t) for w, t in zip(self.weights, self.targets)])
+            self.loss = sum([
+                w * F.mse_loss(gram_matrix(x), t)
+                for w, t in zip(self.weights, self.targets)
+            ])
         return x
 
     def __str__(self):
         return self.weights
-class NeuralStyle(object):
+
+
+class NeuralStyle(torch.nn.Module):
     """Runs the multistyle transfer algorithms
 
   Attributes
@@ -119,10 +130,13 @@ class NeuralStyle(object):
     Runs style transfer via repeatedly doing feedforward and backpropogation
     via lbfgs
     """
-    def __init__(self,
-                 content_layers=['conv_4'],
-                 style_layers=['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']):
-        self.init_model(content_layers, style_layers)
+    def __init__(self, content_layers, style_layers):
+        super(NeuralStyle, self).__init__()
+        self.model, self.content_layers, self.style_layers = self.init_model(
+            content_layers, style_layers)
+
+        self.style_targets = None
+        self.weights = None
 
     def set_style_targets(self, targets, weights):
         with torch.no_grad():
@@ -132,7 +146,9 @@ class NeuralStyle(object):
                 targets = [layer(target) for target in targets]
                 if isinstance(layer, StyleLoss):
                     layer.weights = weights
-                    layer.targets = [gram_matrix(target).detach() for target in targets]
+                    layer.targets = [
+                        gram_matrix(target).detach() for target in targets
+                    ]
 
     @property
     def content_target(self):
@@ -159,11 +175,12 @@ class NeuralStyle(object):
         '''
         # import vgg19 with the pretrained weights
         net = models.vgg19(pretrained=True).features.to(device).eval()
-        net = copy.deepcopy(net)
         # start our sequential model whose first layer in normalization
         model = torch.nn.Sequential()
-        model.add_module('norm_0', Normalization(mean=[0.485, 0.456, 0.406],
-                                                 std=[0.229, 0.224, 0.225]))
+        model.add_module(
+            'norm_0',
+            Normalization(mean=[0.485, 0.456, 0.406],
+                          std=[0.229, 0.224, 0.225]))
         # keep track of the layers to compute style loss and content loss
         style_losses, content_losses = [], []
         i = 0
@@ -200,30 +217,26 @@ class NeuralStyle(object):
 
         # trim the remaining layers proceeding the last content loss or style loss layer
         for i in range(len(model) - 1, -1, -1):
-            if isinstance(model[i], ContentLoss) or isinstance(model[i], StyleLoss):
+            if isinstance(model[i], (ContentLoss, StyleLoss)):
                 break
 
-        self.model = model[:i+1]
-        self.content_layers = content_losses
-        self.style_layers = style_losses
+        return model[:i + 1], content_losses, style_losses
 
     def calculate_losses(self):
         '''Calculates the total loss of an input image
         '''
-        content_loss = sum([loss.loss for loss in self.content_layers]) # / len(self.content_layers)
-        style_loss = sum([loss.loss for loss in self.style_layers]) # / len(self.style_layers)
+        content_loss = sum([lyr.loss for lyr in self.content_layers])
+        style_loss = sum([lyr.loss for lyr in self.style_layers])
 
         return content_loss, style_loss
-
 
     def transfer(self,
                  input_image,
                  epochs=300,
-                 style_weight=1000000,
+                 style_weight=1e6,
                  content_weight=1,
-                 video=False,
-                 silent=True):
-        '''Runs style transfer via repeatedly doing feedforward and backpropogation via lbfgs
+                 verbose=False):
+        """Runs style transfer via repeatedly doing feedforward and backpropogation via lbfgs
 
         Parameters
         ----------
@@ -236,15 +249,16 @@ class NeuralStyle(object):
         The weight of the total style loss function in the total loss function
         content_weight : num
         The weight of the total content loss function in the total loss function
-        '''
+        """
         input_image.requires_grad_()
-        # optimizer = optim.Adam([input_image], lr=1e-1, betas=(0.9, 0.999), eps=1e-08)
         optimizer = optim.LBFGS([input_image])
 
         losses = {'style': [], 'content': [], 'total': []}
-        run = [0]
+        run = [1]
 
+        progressbar = tqdm(total=epochs)
         while run[0] < epochs:
+
             def closure():
                 input_image.data.clamp_(0, 1)
                 optimizer.zero_grad()
@@ -253,7 +267,7 @@ class NeuralStyle(object):
                 total_loss = style_weight * style_loss + content_weight * content_loss
                 total_loss.backward()
 
-                if not silent:
+                if verbose:
                     losses['style'].append(style_loss.item())
                     losses['content'].append(content_loss.item())
                     losses['total'].append(total_loss.item())
@@ -262,8 +276,8 @@ class NeuralStyle(object):
                               f'Style loss: {style_loss} | '
                               f'Content loss: {content_loss} |'
                               f'Total Loss : {total_loss} | ')
-
                 run[0] += 1
+                progressbar.update(1)
                 return total_loss
 
             optimizer.step(closure)
--- neural_multistyle.py
+++ neural_multistyle.py
@@ -1,45 +1,118 @@
 from argparse import ArgumentParser
-from PIL import Image
+import numpy as np
 import torch
-from utils import load_image, to_image
-from model import NST
+import imageio
+from PIL import Image
+import torchvision.transforms as transforms
+from tqdm.auto import trange
+from utils import to_image, image_to_tensor
+from model import NeuralStyle
+
+CONTENT_LAYERS = ['conv_4']
+STYLE_LAYERS = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
+
+def style_transfer_image(args):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    image = Image.open(args.content_target)
+    size = image.size
+    content_target = image_to_tensor(image, size).to(device)
+    style_targets = [
+        image_to_tensor(Image.open(image), size)
+        for image in args.style_targets
+    ]
+    n = len(style_targets)
+    style_weights = np.ones(
+        n) / n if args.style_weights is None else args.style_weights
+    input_image = content_target.clone().to(device)
+
+    neural_style = NeuralStyle(content_layers=CONTENT_LAYERS,
+                               style_layers=STYLE_LAYERS).to(device)
+    neural_style.content_target = content_target
+    neural_style.set_style_targets(style_targets, style_weights)
+
+    output_image = neural_style.transfer(input_image=input_image,
+                                         epochs=args.epochs,
+                                         style_weight=args.style_weight,
+                                         content_weight=args.content_weight,
+                                         verbose=args.verbose)
+
+    to_image(output_image, size=size).save(args.output)
+
+
+def style_transfer_video(args):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    loader = transforms.ToPILImage()
+    reader = imageio.get_reader(args.content_target)
+
+    frames = [
+        image_to_tensor(loader(reader.get_data(i)), (512, 512))
+        for i in range(reader.count_frames())
+    ]
+    style_targets = [
+        image_to_tensor(Image.open(image), (512, 512))
+        for image in args.style_targets
+    ]
+    style_weights = np.linspace(0, 1, num=len(frames))
+
+    neural_style = NeuralStyle(content_layers=CONTENT_LAYERS,
+                               style_layers=STYLE_LAYERS).to(device)
+    input_image = frames[0].to(device)
+    outputs = []
+    for i in trange(len(frames)):
+        neural_style.content_target = frames[i].to(device)
+        neural_style.set_style_targets(
+            style_targets, [1 - style_weights[i], style_weights[i]])
+        output_image = neural_style.transfer(
+            input_image=input_image,
+            epochs=args.epochs,
+            style_weight=args.style_weight,
+            content_weight=args.content_weight,
+            verbose=args.verbose)
+        # del frames[i]
+        input_image = output_image.clone().to(device)
+        outputs.append(output_image.to('cpu'))
+        del output_image
+
+    writer = imageio.get_writer('output.mp4',
+                                fps=reader.get_meta_data()['fps'])
+    shape = reader.get_data(0).shape[:2]
+    outputs = [to_image(output, (shape[1], shape[0])) for output in outputs]
+
+    for output in outputs:
+        writer.append_data(np.asarray(output))
+    writer.close()
+
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('--content',
-                        dest='content_image',
-                        metavar='CONTENT',
-                        required=True)
+    parser.add_argument('--content', dest='content_target', required=True)
     parser.add_argument('--styles',
-                       dest='style_images',
-                       nargs='+',
-                       metavar='STYLES',
-                       required=True)
+                        dest='style_targets',
+                        nargs='+',
+                        required=True)
+    parser.add_argument('--epochs', type=int, dest='epochs', default=1001)
+    parser.add_argument('--output', dest='output', default='output.jpg')
+    parser.add_argument('--content_weight',
+                        type=int,
+                        dest='content_weight',
+                        default=1)
+    parser.add_argument('--style_weight',
+                        type=int,
+                        dest='style_weight',
+                        default=1e6)
     parser.add_argument('--style_weights',
-                       dest='style_weights',
-                       metavar='STYLE_WEIGHTS')
+                        nargs='+',
+                        type=float,
+                        dest='style_weights')
+    parser.add_argument('--verbose', action='store_true', default=False)
     args = parser.parse_args()
-     
-    nst = NST()
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    size = (512, 512) if torch.cuda.is_available() else (128, 128)
-    content_target = load_image(file_name=args.content_image, size=size).to(device)
-    style_targets = [load_image(file_name=name, size=size).to(device) for name in args.style_images]
-    nst = NST()
-    nst.content_target = content_target
-    
-    style_weights = [1 / len(style_targets)] * len(style_targets)
-    nst.set_style_targets(style_targets, style_weights)
-    STYLE_WEIGHT = 1000000
-    CONTENT_WEIGHT = 1
-    EPOCHS = 1001
-    input_image = torch.rand(nst.content_target.shape).to(device)
-    output_image, _ = nst.style_transfer(input_image=input_image,
-                                         epochs=EPOCHS,
-                                         style_weight=STYLE_WEIGHT,
-                                         content_weight=CONTENT_WEIGHT,
-                                         silent=True)
-    to_image(output_image).save('examples/outputs/output.jpg')
+
+    filetype = args.content_target.split('.')
+    if filetype[-1] in ['jpg', 'jpeg', 'png', 'tiff']:
+        style_transfer_image(args)
+    else:
+        style_transfer_video(args)
+
 
 if __name__ == '__main__':
     main()
