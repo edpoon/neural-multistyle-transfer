e122d57e4dee82a8076fec84b9a143cab8236df5
Navies <edward.poon@uwaterloo.ca>
2021-08-26 19:01:16 -0400

    Replace argparser options with Params class

--- neural_multistyle/utils.py
+++ neural_multistyle/utils.py
@@ -1,27 +1,68 @@
-"""Helper module for Pillow image and network tensor conversions
-"""
+"""General utility functions"""
 
+import json
+from typing import Tuple
+from torch import Tensor
 import torchvision.transforms as transforms
 
 
-def image_to_tensor(image, size):
-    """Converts an image to a network tensor
+def image_to_tensor(image: Tensor, size: Tuple[int, int]) -> Tensor:
+    """Converts image to a torch tensor for network input
 
     Args:
-    size (int, int) : Tensor size after converison
+        image: Input image
+        size: Tensor size after converison
     """
-    loader = transforms.Compose(
-        [transforms.Resize(size),
-         transforms.ToTensor()])
+    loader = transforms.Compose([transforms.Resize(size), transforms.ToTensor()])
+    image_tensor = loader(image)
     # add a fake dimension to match VGG19 dimensions
-    return loader(image).unsqueeze(0)
+    image_tensor = image_tensor.unsqueeze(0)
+    return image_tensor
 
 
-def to_image(x, size):
+def to_image(x: Tensor, size: Tuple[int, int]):
     """Convert an output tensor from the network to a PIL image
 
     Args:
-    size (int, int) : Image size after conversion
+        x: Input tensor
+        size: Image size after conversion
     """
+    transform = transforms.ToPILImage()
+    # remove fake dimension used to match VGG19 dimensions
     x = x.cpu().squeeze(0).clone()
-    return transforms.ToPILImage()(x).resize(size)
+    image = transform(x).resize(size)
+    return image
+
+
+class Params:
+    """Class that loads hyperparameters from a json file
+
+    Example:
+    ```
+    params = Params(json_path)
+    print(params.learning_rate)
+    params = learning_rate = 0.5 # change the value of learning_rate in params
+    ```
+    """
+
+    def __init__(self, json_path: str):
+        self.update(json_path)
+
+    def save(self, json_path: str):
+        """Save parameters to json file"""
+        with open(json_path, "w") as f:
+            json.dump(self.__dict__, f, indent=4)
+
+    def update(self, json_path: str):
+        """Load parameters with json file"""
+        with open(json_path) as f:
+            params = json.load(f)
+            self.__dict__.update(params)
+
+    @property
+    def dict(self):
+        """Gives dict-like access to Params instance by `params.dict['learning_rate']`"""
+        return self.__dict__
+
+    def __repr__(self):
+        return repr(self.__dict__)
